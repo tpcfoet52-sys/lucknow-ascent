1cddbc35d087159cb78c9ccdb8e27b87aedcdb90
Yuvika108 <mishrasrishti108@gmail.com>
2026-01-30 15:49:42 +0530

    feat: add three more alumni profile pictures (Vartika Singh, Vinod Mehta, Naresh Trehan)

--- src/pages/OurAlumni.tsx
+++ src/pages/OurAlumni.tsx
@@ -9,6 +9,9 @@ import govindBallabhPant from "@/assets/govind-ballabh-pant.jpg";
 import justiceAsAnand from "@/assets/justice-as-anand.jpg";
 import jagdishGandhi from "@/assets/jagdish-gandhi.jpg";
 import qurratulainHyder from "@/assets/qurratulain-hyder.jpg";
+import vartikaSingh from "@/assets/vartika-singh.jpg";
+import vinodMehta from "@/assets/vinod-mehta.jpg";
+import nareshTrehan from "@/assets/naresh-trehan.jpg";
 
 const alumniLinks = [
     { name: "Alumni Network", href: "#network" },
@@ -27,13 +30,13 @@ const fadeInUp = {
 
 const alumniList = [
     { name: "Ritu Karidhal", role: "Senior Scientist, ISRO (Rocket Woman)", field: "Science & Technology", image: undefined },
-    { name: "Naresh Trehan", role: "Founder, Medanta Hospital (Padma Shri)", field: "Medicine & Healthcare", image: undefined },
+    { name: "Naresh Trehan", role: "Founder, Medanta Hospital (Padma Shri)", field: "Medicine & Healthcare", image: nareshTrehan },
     { name: "S. P. Chakravarti", role: "Pioneer of Electronics Education", field: "Education & Technology", image: spChakravarti },
     { name: "Govind Ballabh Pant", role: "Freedom Fighter & 1st CM of UP", field: "Politics & Governance", image: govindBallabhPant },
     { name: "Justice A. S. Anand", role: "Former Chief Justice of India", field: "Law & Judiciary", image: justiceAsAnand },
     { name: "Qurratulain Hyder", role: "Jnanpith Awardee Novelist", field: "Literature", image: qurratulainHyder },
-    { name: "Vinod Mehta", role: "Eminent Journalist (Editor, Outlook)", field: "Journalism & Media", image: undefined },
-    { name: "Vartika Singh", role: "Indian model and beauty pageant titleholder", field: "Arts & Fashion", image: undefined },
+    { name: "Vinod Mehta", role: "Eminent Journalist (Editor, Outlook)", field: "Journalism & Media", image: vinodMehta },
+    { name: "Vartika Singh", role: "Indian model and beauty pageant titleholder", field: "Arts & Fashion", image: vartikaSingh },
     { name: "Jagdish Gandhi", role: "Founder, City Montessori School", field: "Education", image: jagdishGandhi },
 ];
 
